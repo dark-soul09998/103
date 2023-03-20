e5d7953b5a5f2f90b803320c9b4677031b7254e9
dark-soul09998 <111931211+dark-soul09998@users.noreply.github.com>
2023-03-20 16:37:48 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+https://teachablemachine.withgoogle.com/models/az3J3bPGQ/
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+  });
+
+  camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+      
+function takeSnapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+  console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/az3J3bPGQ/model.json',modelLoaded);
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+  }
+
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+  }
+}
